6ee51880d0155be678f11be78e9bf893e06db4f9
Gouldy-C <mail4clg@gmail.com>
2023-07-20 18:21:01 -0500

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from ConfigModule import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+
+from ConfigModule import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+CORS(app)
+
+jwt = JWTManager(app)
+
+
 
 from app.blueprints.auth import bp as auth
 app.register_blueprint(auth)
